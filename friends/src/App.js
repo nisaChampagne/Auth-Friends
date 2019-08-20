d64a79a93028d6f030db920d8a3adfa047180f4a
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-20 14:19:55 -0400

    axios with auth, privateroute component, login done

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import axios from 'axios'
+import PrivateRoute from './components/PrivateRoute'
+import Login from './components/Login'
+import FriendList from './components/FriendList'
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     <h1> aye</h1>
+      <div>
+        <Link to='/'>Login</Link>
+        {/* <Link to='/friendlist'>FriendList</Link> */}
+        </div>     
+        <div>
+          <Route exact path='/' component={Login} />
+          {/* <PrivateRoute exact path='/friendlist' component={FriendList}/> */}
+        </div>
     </div>
+    </Router>
   );
 }
 
